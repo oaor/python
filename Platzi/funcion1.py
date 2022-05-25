f82807256365aec94e2122222d467e3e417c6e0c
oaor <106263322+oaor@users.noreply.github.com>
2022-05-25 11:59:58 -0500

    Add files via upload
    Ejercicio usando def para definir una variable con condicionantes if, elif y else

---
+++ Platzi/funcion1.py
@@ -0,0 +1,21 @@
+"""
+Trabajemos funciones
+"""
+
+def texto(mensaje):
+    print("Hola")
+    print("¿Cómo Estás?")
+    print(mensaje + str(opcion))
+    print("Adios")
+
+opcion = int(input("Selecciona un número (1, 2 o 3): "))
+
+if opcion == 1:
+    texto("Elegiste la opción ")
+elif opcion == 2:
+    texto("Elegiste la opción ")
+elif opcion == 3:
+    texto("Elegiste la opción ")
+else:
+    print("Escribe una opción correcta")
+    
