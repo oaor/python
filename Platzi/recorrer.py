6d7ff6219429eb648b3d04b2bc45ea78dba11fe0
oaor <106263322+oaor@users.noreply.github.com>
2022-05-25 20:50:44 -0500

    Add files via upload
    Ejercicio usando el ciclo for para recorrer caracteres

---
+++ Platzi/recorrer.py
@@ -0,0 +1,16 @@
+"""
+Cómo hacer un recorrido entre strings usando el ciclo for
+"""
+# Defino la función run
+def run():
+    # nombre = input("Escribe tu nombre: ")
+    # for letra in nombre:
+    #     print(letra)
+    frase = input("Escribe una frase: ")
+    for caracter in frase:
+        print(caracter.upper())
+
+if __name__ == '__main__':
+    run()
+
+
